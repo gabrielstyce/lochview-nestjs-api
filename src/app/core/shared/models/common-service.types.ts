87256e7cd3b2e1c012097ca1c8dd9e986e476419
Gabriel Araujo <gabrielstyce@gmail.com>
2021-11-23 22:28:13 -0300

    Adding delete, active, deactive routines to common  services

--- src/app/core/shared/models/common-service.types.ts
+++ src/app/core/shared/models/common-service.types.ts
@@ -2,8 +2,11 @@ export type IdModel<T> = { id: T };
 export type PromiseDataResponse<T> = Promise<T | null>;
 
 export interface ICommonService<IdTypeDataModel, DataModel, CreateDataModel, UpdateDataModel> {
-  create(dto: CreateDataModel, ...args): PromiseDataResponse<IdModel<IdTypeDataModel>>;
-  update(id: IdTypeDataModel, dto: UpdateDataModel): PromiseDataResponse<IdModel<IdTypeDataModel>>;
-  findById(id: IdTypeDataModel): PromiseDataResponse<DataModel>;
   listAll(): PromiseDataResponse<DataModel[]>;
+  create(dto: CreateDataModel, ...args): PromiseDataResponse<IdModel<IdTypeDataModel>>;
+  update(modelId: IdTypeDataModel, dto: UpdateDataModel): PromiseDataResponse<IdModel<IdTypeDataModel>>;
+  findById(modelId: IdTypeDataModel): PromiseDataResponse<DataModel>;
+  delete(modelId: IdTypeDataModel): PromiseDataResponse<DataModel>;
+  activate(modelId: IdTypeDataModel): PromiseDataResponse<DataModel>;
+  deactivate(modelId: IdTypeDataModel): PromiseDataResponse<DataModel>;
 }
