2fb691459a402053dd1ffcf81599e20ea6f83520
Gabriel Araujo <gabrielstyce@gmail.com>
2021-11-26 02:22:31 -0300

    Adding class-validator global pipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
@@ -7,9 +8,12 @@ const PORT = process.env.PORT || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Prisma config
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app); // Enables graceful shutdown
 
+  // Swagger config
   const config = new DocumentBuilder()
     .setTitle('Lochview API')
     .setDescription('A api that servers to manage an hotel')
@@ -18,6 +22,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
+  // class-validator config
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Application start
   await app.listen(PORT);
   console.log(`Application is RUNING on 'http://localhost:${PORT}'`);
 }
